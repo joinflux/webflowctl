344debd7777ed6c0d06287e787604f624d22d213
Gustavo Hoirisch <gugahoi@users.noreply.github.com>
2023-07-12 23:00:11 +1000

    Add get site command

--- cmd/sites.go
+++ cmd/sites.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"text/tabwriter"
 
 	"github.com/joinflux/webflowctl/internal"
 	"github.com/spf13/cobra"
@@ -11,6 +13,7 @@ import (
 
 func init() {
 	sitesCmd.AddCommand(listSitesCmd)
+	sitesCmd.AddCommand(getSitesCmd)
 	rootCmd.AddCommand(sitesCmd)
 }
 
@@ -38,7 +41,7 @@ type ListSitesResponse []Site
 // listSitesCmd represents the command to list sites in Webflow.
 var listSitesCmd = &cobra.Command{
 	Use:   "list",
-	Short: "create webhooks for a site",
+	Short: "list sites",
 	Run: func(cmd *cobra.Command, args []string) {
 		c := internal.NewClient(ApiToken)
 		body, err := c.Get([]string{"sites"})
@@ -51,5 +54,36 @@ var listSitesCmd = &cobra.Command{
 		for _, site := range response {
 			fmt.Printf("%s\t%s\t%s\t%s\n", site.Name, site.Id, site.LastPublished, site.PreviewUrl)
 		}
+	},
+}
+
+// GetSitesResponse represents a response to the get site request in Webflow.
+// See: https://developers.webflow.com/reference/get-site
+type GetSitesResponse Site
+
+// getSitesCmd represents the command to get a site in Webflow.
+var getSitesCmd = &cobra.Command{
+	Use:   "get [site_id]",
+	Short: "get a site",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		siteId := args[0]
+		c := internal.NewClient(ApiToken)
+		body, err := c.Get([]string{"sites", siteId})
+
+		var response GetSitesResponse
+		err = json.Unmarshal(body, &response)
+		if err != nil {
+			log.Fatalf("Failed to unmarshal response body: %v", err)
+		}
+		w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+		fmt.Fprintf(w, "name:\t%s\n", response.Name)
+		fmt.Fprintf(w, "id:\t%s\n", response.Id)
+		fmt.Fprintf(w, "created on:\t%s\n", response.CreatedOn)
+		fmt.Fprintf(w, "last published:\t%s\n", response.LastPublished)
+		fmt.Fprintf(w, "preview url:\t%s\n", response.PreviewUrl)
+		fmt.Fprintf(w, "short name:\t%s\n", response.ShortName)
+		fmt.Fprintf(w, "timezone:\t%s\n", response.Timezone)
+		w.Flush()
 	},
 }
